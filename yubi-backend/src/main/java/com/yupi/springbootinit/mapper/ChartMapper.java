28807f53c1314810e14aee4daa8db235be2e598c
ambitious <1535820771@qq.com>
2023-09-18 17:02:30 +0800

    初始化数据库，后端生成增删改查代码

---
+++ yubi-backend/src/main/java/com/yupi/springbootinit/mapper/ChartMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author ambitious
+* @description 针对表【chart(图表信息)】的数据库操作Mapper
+* @createDate 2023-09-18 16:51:59
+* @Entity com.yupi.springbootinit.model.entity.Chart
+*/
+public interface ChartMapper extends BaseMapper<Chart> {
+
+}
+
+
+
+
---
+++ yubi-backend/src/main/java/com/yupi/springbootinit/service/ChartService.java
@@ -0,0 +1,13 @@
+package com.yupi.springbootinit.service;
+
+import com.yupi.springbootinit.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author ambitious
+* @description 针对表【chart(图表信息)】的数据库操作Service
+* @createDate 2023-09-18 16:51:59
+*/
+public interface ChartService extends IService<Chart> {
+
+}
